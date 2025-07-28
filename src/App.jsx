4ab083ad0967a1b04afaa89e543459720729977c
isaacmorais903-oss <isaacmorais903@gmail.com>
2025-07-28 17:35:19 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Tabela de ICMS por Estado - 2025</h1>
+      <p>Projeto React + Vite configurado com sucesso!</p>
+    </div>
+  );
+}
+
+export default App;
